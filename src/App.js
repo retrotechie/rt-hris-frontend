daedcc3fb0ecf0bd769cd1a03adbd35d23ac5ee4
Retro Techie <107520616+retrotechie@users.noreply.github.com>
2023-01-05 14:28:27 -0800

    feat: configure Routing
    Implements [React Router Dom] to configure Routing. `react-router-dom`
    is used to build single-page applications (applications that have many
    pages or components but the page is never refreshed instead the content
    is dynamically fetched based on the URL).
    
    The major advantage of `react-router-dome` is that the page doesn't have
    to be refreshed when a link to another page is clicked. It's very
    fast compared to traditional page navigation.
    
    Wrap up `Navbar` and `Home` in `Router`.
    
    Issue retrotechie/rt-hris#31
    Link:
    - [React Router Dom](https://www.npmjs.com/package/react-router-dom)
    Reference:
    - [What is React Router Dome](https://www.geeksforgeeks.org/what-is-react-router-dom/)

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './layout/Navbar';
 import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
