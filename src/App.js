d5a32de728d82fa3ba0d4dba059edc2283e7f6da
Retro Techie <107520616+retrotechie@users.noreply.github.com>
2023-01-07 16:57:05 -0800

    feat: perform `AddEmployee`
    Update `Add Employee` button in `Navbar` to link to `/addemployee` path that runs
    `AddEmployee` function.
    
    Add `AddEmployee` as a `Route` to App.js `Router`.
    
    Issue retrotechie/rt-hris#39

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './layout/Navbar';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AddEmployee from './employee/AddEmployee';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path='/' element={<Home />} />
+          <Route exact path='/addemployee' element={<AddEmployee />} />
         </Routes>
       </Router>
     </div>
---
+++ src/employee/AddEmployee.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AddEmployee() {
+  return (
+    <div>AddEmployee Form</div>
+  )
+}
