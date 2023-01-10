392fae8304f050f15d5d2afd40b81195e33fb8ea
Retro Techie <107520616+retrotechie@users.noreply.github.com>
2023-01-09 22:16:24 -0800

    feat: create `AddEmployee` form
    `AddEmployee` returns a form to create new employee on `/addemployee`.
    
    `htmlFor` is originally `for` in HTML. It has a value which is the `id`
    of the form element it relates to.
    
    Issue retrotechie/rt-hris#39
    References:
    - [DOM Elements htmlFor](https://reactjs.org/docs/dom-elements.html#htmlfor)
    - [HTML attribute for](https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/for)
    - [Label element](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/label)
    - [Attribute for](https://www.w3schools.com/tags/att_for.asp)
    See also:
    - [Difference between id and name attributes in HTML](https://stackoverflow.com/questions/1397592/difference-between-id-and-name-attributes-in-html)

--- src/employee/AddEmployee.jsx
+++ src/employee/AddEmployee.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import React from 'react';
 
 export default function AddEmployee() {
   return (
-    <div>AddEmployee Form</div>
-  )
-}
+    <div className='container'>
+      <div className='row'>
+        <div className='col-md-6 offset-md-3 border rounded p-3 mt-2 shadow'>
+          <h2 className='text-center m-4'>Add Employee</h2>
+          <div className='mb-3'>
+            <label htmlFor='FirstName' className='form-label'>First Name</label>
+            <input
+              type={"text"}
+              className="form-control"
+              placeholder='Enter first name'
+              name='firstName'
+              id='FirstName'
+            >
+            </input>
+          </div>
+          <div className='mb-3'>
+            <label htmlFor='LastName' className='form-label'>Last Name</label>
+            <input
+              type={"text"}
+              className="form-control"
+              placeholder='Enter last name'
+              name='lastName'
+              id='LastName'
+            >
+            </input>
+          </div>
+          <div className='mb-3'>
+            <label htmlFor='Email' className='form-label'>E-mail</label>
+            <input
+              type={"text"}
+              className="form-control"
+              placeholder='Enter e-mail address'
+              name='email'
+              id='Email'
+            >
+            </input>
+          </div>
+          <button type='submit' className='btn btn-outline-primary'>Submit</button>
+          <button type='submit' className='btn btn-outline-danger mx-2'>Cancel</button>
+        </div>
+      </div>
+    </div>
+  );
+};
